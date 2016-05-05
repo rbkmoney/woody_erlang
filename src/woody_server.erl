3f97597a7913700751709c9e46cd182d4885a21d
Anton Belyaev <xantbel@gmail.com>
2016-05-05 08:57:25 +0300

    Remove unnecessary wrapper supervisor for woody_server

--- src/woody_server.erl
+++ src/woody_server.erl
@@ -3,13 +3,8 @@
 
 -module(woody_server).
 
--behaviour(supervisor).
-
 %% API
 -export([child_spec/2]).
-
-%% supervisor callbacks
--export([init/1]).
 
 %%
 %% behaviour definition
@@ -27,24 +22,4 @@
 -spec child_spec(_Id, options()) -> supervisor:child_spec().
 child_spec(Id, Options) ->
     ProtocolHandler = woody_t:get_protocol_handler(server, Options),
-    ServerSpec = ProtocolHandler:child_spec(Id, Options),
-    #{
-        id       => Id,
-        start    => {supervisor, start_link, [?MODULE, {woody_server, ServerSpec}]},
-        restart  => permanent,
-        shutdown => infinity,
-        type     => supervisor,
-        modules  => [?MODULE]
-    }.
-
-%%
-%% Supervisor callbacks
-%%
--spec init({woody_server, supervisor:child_spec()}) -> {ok, {#{}, [#{}, ...]}}.
-init({woody_server, ChildSpec}) ->
-    {ok, {#{
-        strategy  => one_for_one,
-        intensity => 10,
-        period    => 10},
-    [ChildSpec]}
-}.
+    ProtocolHandler:child_spec(Id, Options).
