f41a0a55a8713ace381db1f9c9d01d2db0af0a1c
Anton Belyaev <xantbel@gmail.com>
2016-08-25 14:56:19 +0300

    MSPF-61: add woody_client:make_id_int/0 API function

--- src/woody_client.erl
+++ src/woody_client.erl
@@ -11,6 +11,7 @@
 -export([new_context/2]).
 -export([get_rpc_id/1]).
 -export([make_id/1]).
+-export([make_id_int/0]).
 -export([make_child_context/2]).
 
 -export([call/3]).
@@ -141,11 +142,16 @@ call_async(Sup, Callback, Context, Request, Options) ->
     supervisor:start_child(ClientSup,
         [Callback, Context, Request, Options]).
 
+-spec make_id_int() -> pos_integer().
+make_id_int() ->
+    <<Id:64>> = snowflake:new(?MODULE),
+    Id.
+
 -spec make_id(binary()) -> woody_t:req_id().
 make_id(Suffix) when is_binary(Suffix) ->
-    <<SfInt:64>> = snowflake:new(?MODULE),
-    SfBin = genlib:to_binary(SfInt),
-    <<SfBin/binary, $:, Suffix/binary>>.
+    IdInt = make_id_int(),
+    IdBin = genlib:to_binary(IdInt),
+    <<IdBin/binary, $:, Suffix/binary>>.
 
 %%
 %% Internal API
