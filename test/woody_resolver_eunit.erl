e11736cda5d4c9e1a9c4664ccd0c466a6a31b6e3
Kehitt <devstealth@gmail.com>
2019-03-14 16:08:05 +0300

    MSPF-416: Fix linting

--- test/woody_resolver_eunit.erl
+++ test/woody_resolver_eunit.erl
@@ -4,22 +4,42 @@
 
 resolve_ipv4_test() ->
     ?assertEqual(ok, inet_db:set_inet6(false)),
-    ?assertEqual({ok, <<"http://127.0.0.1:80/test">>}, woody_resolver:resolve_url(<<"localhost/test">>)),
-    ?assertEqual({ok, <<"http://127.0.0.1:80/test">>}, woody_resolver:resolve_url(<<"http://localhost/test">>)),
-    ?assertEqual({ok, <<"https://127.0.0.1:8080/test">>}, woody_resolver:resolve_url(<<"https://localhost:8080/test">>)),
-    ?assertEqual({ok, <<"https://127.0.0.1:443/">>}, woody_resolver:resolve_url(<<"https://localhost">>)).
+    ?assertEqual(
+        {ok, <<"http://127.0.0.1:80/test">>},
+        woody_resolver:resolve_url(<<"localhost/test">>)
+    ),
+    ?assertEqual(
+        {ok, <<"http://127.0.0.1:80/test">>},
+        woody_resolver:resolve_url(<<"http://localhost/test">>)
+    ),
+    ?assertEqual(
+        {ok, <<"https://127.0.0.1:8080/test">>},
+        woody_resolver:resolve_url(<<"https://localhost:8080/test">>)
+    ),
+    ?assertEqual(
+        {ok, <<"https://127.0.0.1:443/">>},
+        woody_resolver:resolve_url(<<"https://localhost">>)
+    ).
 
 resolve_ipv6_test() ->
     ?assertEqual(ok, inet_db:set_inet6(true)),
-    ?assertEqual({ok, <<"http://[::1]:80/test">>}, woody_resolver:resolve_url(<<"localhost/test">>)),
-    ?assertEqual({ok, <<"http://[::1]:80/test">>}, woody_resolver:resolve_url(<<"http://localhost/test">>)),
-    ?assertEqual({ok, <<"http://[::1]:8080/test?q">>}, woody_resolver:resolve_url(<<"http://localhost:8080/test?q">>)),
-    ?assertEqual({ok, <<"https://[::1]:443/">>}, woody_resolver:resolve_url(<<"https://localhost">>)).
+    ?assertEqual(
+        {ok, <<"http://[::1]:80/test">>},
+        woody_resolver:resolve_url(<<"localhost/test">>)
+    ),
+    ?assertEqual(
+        {ok, <<"http://[::1]:80/test">>},
+        woody_resolver:resolve_url(<<"http://localhost/test">>)
+    ),
+    ?assertEqual(
+        {ok, <<"http://[::1]:8080/test?q">>},
+        woody_resolver:resolve_url(<<"http://localhost:8080/test?q">>)
+    ),
+    ?assertEqual(
+        {ok, <<"https://[::1]:443/">>},
+        woody_resolver:resolve_url(<<"https://localhost">>)
+    ).
 
 resolve_errors_test() ->
     ?assertEqual({error, unable_to_resolve_host}, woody_resolver:resolve_url(<<"http://nxdomainme">>)),
-    %% hackney_url:parse_url just assumes any unrecognized url scheme to be http and ends up resolving something like
-    %% http://ftp://localhost (hostname being ftp), which in turn nxdomains
-    %% A fix for this would be to match for <<"http(s)://", _Rest/binary>> in resolve_url, but since I'm not certain
-    %% about us relying on this logic in production (in tests we do by using 0.0.0.0/.., for example) I decided against it
     ?assertEqual({error, unable_to_resolve_host}, woody_resolver:resolve_url(<<"ftp://localhost">>)).
