afd2c0199a2b7797f4beb5cc8ae2dae565f1b8e0
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-06-26 14:14:24 +0300

    Removed filtering, added key tagging

--- src/woody_client_metrics.erl
+++ src/woody_client_metrics.erl
@@ -15,31 +15,26 @@
 -type metric() :: counter | histogram | gauge | meter.
 
 -spec new(metric(), any()) -> ok | {error, term()}.
-new(histogram, Key) ->
-    new(gauge, Key); % so far we don't want histograms
-new(meter, _) ->
-    {error, not_allowed};
-new(Type, Key) ->
-    case validate_metric(Key) of
-        true ->
-            hay_metrics:register(hay_metrics:construct(Type, Key, 0));
-        false ->
-            {error, not_allowed}
-    end.
+new(_, _) ->
+    ok. % we can omit it
 
 -spec delete(any()) -> ok.
 delete(_) ->
-    ok. % Can't we delete metrics in HAY?
+    ok.
 
 -spec increment_counter(any()) -> ok | {error, term()}.
 increment_counter(Key) ->
     increment_counter(Key, 1).
 
 -spec increment_counter(any(), number()) -> ok | {error, term()}.
+increment_counter([hackney, _Host, _], _) ->
+    ok; % we don't need per host metrics
+increment_counter([hackney, nb_requests], Value) ->
+    increment_counter([hackney, requests_in_process], Value);
 increment_counter(Key, Value) ->
     case validate_metric(Key) of
         true ->
-            hay_metrics:push(hay_metrics:construct(counter, Key, Value));
+            hay_metrics:push(hay_metrics:construct(counter, tag_key(Key), Value));
         false ->
             {error, not_allowed}
     end.
@@ -50,7 +45,7 @@ decrement_counter(Key) ->
 
 -spec decrement_counter(any(), number()) -> ok | {error, term()}.
 decrement_counter(Key, Value) ->
-    increment_counter(Key, -Value). % Should work tho
+    increment_counter(Key, -Value).
 
 -spec update_histogram(any(), number() | function()) -> ok | {error, term()}.
 update_histogram(_, Func)  when is_function(Func) ->
@@ -62,7 +57,7 @@ update_histogram(Key, Value) ->
 update_gauge(Key, Value) ->
     case validate_metric(Key) of
         true ->
-            hay_metrics:push(hay_metrics:construct(gauge, Key, Value));
+            hay_metrics:push(hay_metrics:construct(gauge, tag_key(Key), Value));
         false ->
             {error, not_allowed}
     end.
@@ -71,41 +66,20 @@ update_gauge(Key, Value) ->
 update_meter(_, _) ->
     {error, not_allowed}.
 
-
 %% internals
 
-validate_metric([hackney, Host, Metric]) ->
-    is_allowed_host(Host) andalso validate_metric([hackney, Metric]);
-validate_metric([hackney_pool, Pool, Metric]) ->
-    is_allowed_pool(Pool) andalso is_allowed_metric(Metric);
-validate_metric([_, Metric]) ->
-    is_allowed_metric(Metric).
+tag_key(Key) when is_list(Key) ->
+    [woody, client | Key];
+tag_key(Key) ->
+    [woody, client, Key].
 
-% is_allowed_*
-
-is_allowed_pool(Host) ->
-    case get_allowed_pools() of
-        '*' -> true;
-        Allowed -> lists:member(Host, Allowed)
-    end.
-
-is_allowed_host(Host) ->
-    case get_allowed_hosts() of
-        '*' -> true;
-        Allowed -> lists:member(Host, Allowed)
-    end.
+validate_metric(Key) ->
+    is_allowed_metric(lists:last(Key)).
 
 is_allowed_metric(Key) ->
     lists:member(Key, get_allowed_metrics()).
 
 % gets
-
-% maybe we shouldn't access env so frequently?
-get_allowed_pools() ->
-    maps:get(allowed_pools, get_options(), []).
-
-get_allowed_hosts() ->
-    maps:get(allowed_hosts, get_options(), []).
 
 get_allowed_metrics() ->
     maps:get(allowed_metrics, get_options(), []).
