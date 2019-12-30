89cd4b628dde814670ba84829214d7c3726e98d7
Andrew Mayorov <a.mayorov@rbkmoney.com>
2019-12-30 16:48:04 +0300

    Update src/woody_event_formatter.erl
    Co-Authored-By: Sergey Yelin <elinsn@gmail.com>

--- src/woody_event_formatter.erl
+++ src/woody_event_formatter.erl
@@ -96,7 +96,7 @@ format_exception(_Module, _Service, _Function, Value, Opts) ->
 
 format(ReplyType, Value, #{max_length := ML, max_depth := MD, max_pritable_string_length := MPSL}) ->
     try
-        ReplyFmt = format_thrift_value(ReplyType, Value, <<>>, MD, ML, MPSL),
+        ReplyValue = format_thrift_value(ReplyType, Value, <<>>, MD, ML, MPSL),
         {"~ts", [ReplyFmt]}
     catch
         E:R:S ->
